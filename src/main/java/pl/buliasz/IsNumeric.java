3d00801fdb11d360bacf3f481a690aaf5b9fe11e
blazej <blazej.uliasz2004@gmail.com>
2025-10-06 15:50:56 +0200

    added function to check if string is number

---
+++ src/main/java/pl/buliasz/IsNumeric.java
@@ -0,0 +1,12 @@
+package pl.buliasz;
+
+public class IsNumeric {
+    public static boolean isNumeric(String str) {
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch(NumberFormatException e){
+            return false;
+        }
+    }
+}
