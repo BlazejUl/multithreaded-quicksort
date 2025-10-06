e88a5e6a756ce0ce04c5d5b2929d8d707214679e
blazej <blazej.uliasz2004@gmail.com>
2025-10-06 15:49:15 +0200

    added some user interface

--- src/main/java/pl/buliasz/Main.java
+++ src/main/java/pl/buliasz/Main.java
@@ -1,14 +1,38 @@
 package pl.buliasz;
 
 import java.util.Random;
+import java.util.Scanner;
 import java.util.concurrent.ForkJoinPool;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         int[] arr = {7,1,5,8,3,6};
         int end = arr.length-1,
         numberOfSimulations = 2,
         maxThreadCap=4;
+        boolean isOn = true, arrayOutput = true, acceptableUserOutput = false;
+
+        System.out.println("""
+                Aplikacja wyczyści pliki jeżeli
+                 nie chcesz stracić danych skopiuj je do innych plików
+                (naciśnij enter)""");
+        scanner.nextLine();
+
+        System.out.println("""
+                    
+                     Aby zobaczyć w plikach nie posortowaną oraz posortowaną listę
+                     liczba symulacji nie może przekraczać 10 a długość listy 100
+                     
+                     """);
+        while (acceptableUserOutput = false){
+            System.out.println("Podaj ilośc symulacji do wykonania");
+            String userOutput = scanner.nextLine();
+            if ()
+        }
+
+
+
         ForkJoinPool pool = ForkJoinPool.commonPool();
         pool.setParallelism(maxThreadCap);
 
