2a7b1185da5ca4eb9a1d27e688bb294244b283d6
blazej <blazej.uliasz2004@gmail.com>
2025-10-06 16:33:53 +0200

    added more user textinterface

--- src/main/java/pl/buliasz/Main.java
+++ src/main/java/pl/buliasz/Main.java
@@ -3,15 +3,13 @@ package pl.buliasz;
 import java.util.Random;
 import java.util.Scanner;
 import java.util.concurrent.ForkJoinPool;
+import static pl.buliasz.IsNumeric.isNumeric;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int[] arr = {7,1,5,8,3,6};
-        int end = arr.length-1,
-        numberOfSimulations = 2,
-        maxThreadCap=4;
-        boolean isOn = true, arrayOutput = true, acceptableUserOutput = false;
+        int numberOfSimulations = 1, maxThreadCap = 4, arrLength = 0;
+        boolean isOn = true, writeArrayOutput = false, acceptableUserOutput = false;
 
         System.out.println("""
                 Aplikacja wyczyści pliki jeżeli
@@ -25,13 +23,41 @@ public class Main {
                      liczba symulacji nie może przekraczać 10 a długość listy 100
                      
                      """);
-        while (acceptableUserOutput = false){
+
+        while (acceptableUserOutput == false){
             System.out.println("Podaj ilośc symulacji do wykonania");
             String userOutput = scanner.nextLine();
-            if ()
+            if (isNumeric(userOutput)){
+                numberOfSimulations = Integer.parseInt(userOutput);
+                acceptableUserOutput = true;}
+            else {System.out.println("Podano nieprawidłową wartość");}
         }
 
+        acceptableUserOutput = false;
+        while (acceptableUserOutput == false){
+            System.out.println("Podaj długość tablicy do posortowania");
+            String userOutput = scanner.nextLine();
+            if (isNumeric(userOutput)){
+                arrLength = Integer.parseInt(userOutput);
+                acceptableUserOutput = true;}
+            else {System.out.println("Podano nieprawidłową wartość");}
+        }
+        int[] arr = new int[arrLength];
+        int end = arr.length-1;
 
+        arr  = ArrRandomiser.fillArrayRandom(arr);
+
+        if(arrLength <= 100 && numberOfSimulations <= 10) writeArrayOutput = true;
+
+        acceptableUserOutput = false;
+        while (acceptableUserOutput == false){
+            System.out.println("Podaj maksymalną ilość wątków");
+            String userOutput = scanner.nextLine();
+            if (isNumeric(userOutput)){
+                maxThreadCap = Integer.parseInt(userOutput);
+                acceptableUserOutput = true;}
+            else {System.out.println("Podano nieprawidłową wartość");}
+        }
 
         ForkJoinPool pool = ForkJoinPool.commonPool();
         pool.setParallelism(maxThreadCap);
