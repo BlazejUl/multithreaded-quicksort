235f5f9bbfee4f7b94fb87bf097399199bdae0bd
blazej <blazej.uliasz2004@gmail.com>
2025-10-06 16:19:22 +0200

    small tweak to fill array random

--- src/main/java/pl/buliasz/ArrRandomiser.java
+++ src/main/java/pl/buliasz/ArrRandomiser.java
@@ -3,9 +3,8 @@ package pl.buliasz;
 import java.util.Random;
 
 public class ArrRandomiser {
-    public static int[] CreateRandomArray(int length){
-        int[] arr = new int[length];
-        for(int i=0;i<length;i++){
+    public static int[] fillArrayRandom(int[] arr){
+        for(int i=0;i <= arr.length-1;i++){
             arr[i] = new Random().nextInt(1_000_000);
         }
         return arr;
